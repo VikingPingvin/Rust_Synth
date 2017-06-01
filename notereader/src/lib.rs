0ea96b967d63410c5d6cf79e7011f778e7b15806
VikingPingvin <sch1458@gmail.com>
2017-06-01 15:33:54 +0200

    Fix getfreq parameter error.

--- notereader/src/lib.rs
+++ notereader/src/lib.rs
@@ -12,7 +12,7 @@ pub fn note_to_freq(notestr:&str){		//Read notes from file |||TEST PURPOSES|||
 	println!("Note: {}, Octave: {}",notebytes[0] as char, notebytes[1] as char);
 }
 	
-pub fn getfreq(note:char,mod:char,oct:i8){	//Input a note and get the freq for it
+pub fn getfreq(note:char,modif:char,oct:i8){	//Input a note and get the freq for it
 	//_getfreq->
 }
 fn _getfreq(note:char,oct:i8){
