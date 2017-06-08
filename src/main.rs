49f0a4cfc6758bc98de3128414b0554a199bfd0b
Artúr Kovács <kovacs.artur.barnabas@gmail.com>
2017-06-08 22:20:10 +0200

    Added the basis for manipulating the pitch during playback

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,10 @@ use futures::task::Executor;
 use futures::task::Run;
 
 use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
-
+use std::vec;
 
 
 struct MyExecutor;
@@ -22,10 +23,51 @@ impl Executor for MyExecutor {
     }
 }
 
+
+struct SamplesIter<'a> {
+    synth: &'a mut Synthesizer
+}
+
+impl<'a> Iterator for SamplesIter<'a> {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.synth.next_sampe())
+    }
+}
+
+
+struct Synthesizer {
+    sample_rate: u32,
+    channel_count: u8,
+    curr_freq: f32,
+    curr_sample: u64,
+    volume: f32
+}
+
+impl Synthesizer {
+    fn new() -> Synthesizer {
+        Synthesizer {sample_rate: 44100, channel_count: 2, curr_freq: 440.0, curr_sample: 0, volume: 0.25}
+    }
+
+    fn samples_iter(&mut self) -> SamplesIter {
+        SamplesIter {synth: self}
+    }
+
+    fn next_sampe(&mut self) -> f32 {
+        use std::f64::consts::PI;
+        let curr_sec = self.curr_sample as f64 / self.sample_rate as f64;
+        self.curr_sample += 1;
+        return self.volume * ((PI * 2.0 * curr_sec) as f32 * self.curr_freq).sin()
+    }
+}
+
+
 fn main() {
 
     playbeep();
 }
+
 
 fn playbeep(){
     let endpoint = cpal::get_default_endpoint().expect("Failed to get default endpoint");
@@ -49,41 +91,81 @@ fn playbeep(){
 
     Voice_Hz+=10.0;
 
+    let mut synth_arc_mtx = Arc::new(Mutex::new(Synthesizer::new()));
+    {
+        let mut synth = synth_arc_mtx.lock().unwrap();
+        synth.sample_rate = format.samples_rate.0;
+        synth.channel_count = format.channels.len() as u8;
+    } // mutex gets unlocked here
+
     voice.play();
-    task::spawn(stream.for_each(move |buffer| -> Result<_, ()> {
-        match buffer {
-            cpal::UnknownTypeBuffer::U16(mut buffer) => {
-                for (sample, value) in buffer.chunks_mut(format.channels.len()).zip(&mut data_source) {
-                    let value = ((value * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
-                    for out in sample.iter_mut() { *out = value; }
-                }
-            },
+    {
+        // creating a new reference counted reference to synth
+        let buffer_thread_synth = synth_arc_mtx.clone();
+        task::spawn(stream.for_each(move |buffer| -> Result<_, ()> {
+            let mut synth = buffer_thread_synth.lock().unwrap();
+            match buffer {
+                cpal::UnknownTypeBuffer::U16(mut buffer) => {
+                    for (sample, value) in buffer.chunks_mut(synth.channel_count as usize).zip(synth.samples_iter()) {
+                        let value = ((value * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
+                        for out in sample.iter_mut() { *out = value; }
+                    }
+                },
 
-            cpal::UnknownTypeBuffer::I16(mut buffer) => {
-                for (sample, value) in buffer.chunks_mut(format.channels.len()).zip(&mut data_source) {
-                    let value = (value * std::i16::MAX as f32) as i16;
-                    for out in sample.iter_mut() { *out = value; }
-                }
-            },
+                cpal::UnknownTypeBuffer::I16(mut buffer) => {
+                    for (sample, value) in buffer.chunks_mut(synth.channel_count as usize).zip(synth.samples_iter()) {
+                        let value = (value * std::i16::MAX as f32) as i16;
+                        for out in sample.iter_mut() { *out = value; }
+                    }
+                },
 
-            cpal::UnknownTypeBuffer::F32(mut buffer) => {
-                for (sample, value) in buffer.chunks_mut(format.channels.len()).zip(&mut data_source) {
-                    for out in sample.iter_mut() { *out = value; }
-                }
-            },
-        };
+                cpal::UnknownTypeBuffer::F32(mut buffer) => {
+                    for (sample, value) in buffer.chunks_mut(synth.channel_count as usize).zip(synth.samples_iter()) {
+                        for out in sample.iter_mut() { *out = value; }
+                    }
+                },
+            };
 
-        Ok(())
-    })).execute(executor);
+            Ok(())
+        })).execute(executor);
+    }
+
 	notereader::note_to_freq("A2");
-    thread::spawn(move || {
-        loop {
-            //thread::sleep(Duration::from_millis(300));
-            //voice.pause();
-            //thread::sleep(Duration::from_millis(30));
-            voice.play();
-        }
-    });
+
+    let freqs = [
+        440.0,
+        466.1637615180899,
+        493.8833012561241,
+        523.2511306011972,
+        554.3652619537442,
+        587.3295358348151,
+        622.2539674441618,
+        659.2551138257398,
+        698.4564628660078,
+        739.9888454232688,
+        783.9908719634985,
+        830.6093951598903,
+    ];
+    let mut curr_freq_id = 0;
+
+    {
+        // creating a new reference counted reference to synth
+        let control_thread_synth = synth_arc_mtx.clone();
+        thread::spawn(move || {
+            loop {
+                {
+                    let mut synth = control_thread_synth.lock().unwrap();
+                    synth.curr_freq = freqs[curr_freq_id];
+                }
+                voice.play();
+                curr_freq_id = (curr_freq_id+1) % freqs.len();
+                if curr_freq_id == 0 {
+                    voice.pause();
+                }
+                thread::sleep(Duration::from_millis(500));
+            }
+        });
+    }
 
     event_loop.run();
 }
