3804d91c1ad7790a4bead94eb09156a3e0e5cfb3
Artúr Kovács <kovacs.artur.barnabas@gmail.com>
2017-06-10 14:01:31 +0200

    Cleaned up main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
-use std::vec;
 
 
 struct MyExecutor;
@@ -24,47 +23,68 @@ impl Executor for MyExecutor {
 }
 
 
-struct SamplesIter<'a> {
+/// Iterator for iterating over Synthesizer samples.
+///
+/// It is possible to request a SampleIter from a Synthesizer instance.
+/// This SampleIter instance will hold a mutable reference to the Synthesizer
+/// and in each iteration get the next sample.
+///
+/// Please note that while a SampleIter exists for a Synthesizer, the state
+/// of that synthesizer can not be chaned.
+struct SampleIter<'a> {
     synth: &'a mut Synthesizer
 }
 
-impl<'a> Iterator for SamplesIter<'a> {
+impl<'a> Iterator for SampleIter<'a> {
     type Item = f32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        Some(self.synth.next_sampe())
+        return Some(self.synth.next_sampe())
     }
 }
 
 
+/// The Synthesizer itself.
+///
+/// Holds all the necessary data to generate the sound samples from the playing notes.
 struct Synthesizer {
     sample_rate: u32,
     channel_count: u8,
-    curr_freq: f32,
+    curr_freq: f64,
     curr_sample: u64,
     volume: f32
 }
 
 impl Synthesizer {
     fn new() -> Synthesizer {
-        Synthesizer {sample_rate: 44100, channel_count: 2, curr_freq: 440.0, curr_sample: 0, volume: 0.25}
+        return Synthesizer {sample_rate: 44100, channel_count: 2, curr_freq: 440.0, curr_sample: 0, volume: 0.1}
     }
 
-    fn samples_iter(&mut self) -> SamplesIter {
-        SamplesIter {synth: self}
+    /// Gets an iterator that gives the next sample in each iteration.
+    /// (each iteration calls next_sample)
+    fn samples_iter(&mut self) -> SampleIter {
+        return SampleIter {synth: self}
     }
 
+    /// Gets the next sample in the audio signal.
+    ///
+    /// This function will return a value in range [-1, 1] denoting the required
+    /// position of the membrane in order to play the sound. The corresponding time point is
+    /// calculated from the sample rate of the synthesizer.
+    ///
+    /// Each call will step the synthesizer to the following sample so that consecutive calls
+    /// return consecutive samples.
     fn next_sampe(&mut self) -> f32 {
         use std::f64::consts::PI;
         let curr_sec = self.curr_sample as f64 / self.sample_rate as f64;
         self.curr_sample += 1;
-        return self.volume * ((PI * 2.0 * curr_sec) as f32 * self.curr_freq).sin()
+        return self.volume * (PI * 2.0 * curr_sec * self.curr_freq).sin() as f32;
     }
 }
 
 
-fn main() {
 
+fn main() {
     playbeep();
 }
 
@@ -76,22 +96,12 @@ fn playbeep(){
     let event_loop = cpal::EventLoop::new();
     let executor = Arc::new(MyExecutor);
 
-    let mut currtime = time::get_time();
-    println!("Time is: {}",5);
+    let curr_time = time::get_time();
+    println!("Time is: {}", curr_time.sec);
 
     let (mut voice, stream) = cpal::Voice::new(&endpoint, &format, &event_loop).expect("Failed to create a voice");
 
-    // Produce a sinusoid of maximum amplitude.
-    let samples_rate = format.samples_rate.0 as f32;
-
-    let mut Voice_Hz = 300.0;
-
-    let mut data_source = (0u64..).map(move |t| t as f32 * Voice_Hz * 2.0 * 3.141592 / samples_rate)     // 440 Hz
-        .map(move |t| t.sin()*0.1);
-
-    Voice_Hz+=10.0;
-
-    let mut synth_arc_mtx = Arc::new(Mutex::new(Synthesizer::new()));
+    let synth_arc_mtx = Arc::new(Mutex::new(Synthesizer::new()));
     {
         let mut synth = synth_arc_mtx.lock().unwrap();
         synth.sample_rate = format.samples_rate.0;
@@ -99,36 +109,34 @@ fn playbeep(){
     } // mutex gets unlocked here
 
     voice.play();
-    {
-        // creating a new reference counted reference to synth
-        let buffer_thread_synth = synth_arc_mtx.clone();
-        task::spawn(stream.for_each(move |buffer| -> Result<_, ()> {
-            let mut synth = buffer_thread_synth.lock().unwrap();
-            match buffer {
-                cpal::UnknownTypeBuffer::U16(mut buffer) => {
-                    for (sample, value) in buffer.chunks_mut(synth.channel_count as usize).zip(synth.samples_iter()) {
-                        let value = ((value * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
-                        for out in sample.iter_mut() { *out = value; }
-                    }
-                },
+    // creating a new reference counted reference to synth
+    let buffer_thread_synth = synth_arc_mtx.clone();
+    task::spawn(stream.for_each(move |buffer| -> Result<_, ()> {
+        let mut synth = buffer_thread_synth.lock().unwrap();
+        match buffer {
+            cpal::UnknownTypeBuffer::U16(mut buffer) => {
+                for (sample, value) in buffer.chunks_mut(synth.channel_count as usize).zip(synth.samples_iter()) {
+                    let value = ((value * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
+                    for out in sample.iter_mut() { *out = value; }
+                }
+            },
 
-                cpal::UnknownTypeBuffer::I16(mut buffer) => {
-                    for (sample, value) in buffer.chunks_mut(synth.channel_count as usize).zip(synth.samples_iter()) {
-                        let value = (value * std::i16::MAX as f32) as i16;
-                        for out in sample.iter_mut() { *out = value; }
-                    }
-                },
+            cpal::UnknownTypeBuffer::I16(mut buffer) => {
+                for (sample, value) in buffer.chunks_mut(synth.channel_count as usize).zip(synth.samples_iter()) {
+                    let value = (value * std::i16::MAX as f32) as i16;
+                    for out in sample.iter_mut() { *out = value; }
+                }
+            },
 
-                cpal::UnknownTypeBuffer::F32(mut buffer) => {
-                    for (sample, value) in buffer.chunks_mut(synth.channel_count as usize).zip(synth.samples_iter()) {
-                        for out in sample.iter_mut() { *out = value; }
-                    }
-                },
-            };
+            cpal::UnknownTypeBuffer::F32(mut buffer) => {
+                for (sample, value) in buffer.chunks_mut(synth.channel_count as usize).zip(synth.samples_iter()) {
+                    for out in sample.iter_mut() { *out = value; }
+                }
+            },
+        };
 
-            Ok(())
-        })).execute(executor);
-    }
+        Ok(())
+    })).execute(executor);
 
 	notereader::note_to_freq("A2");
 
@@ -148,24 +156,23 @@ fn playbeep(){
     ];
     let mut curr_freq_id = 0;
 
-    {
-        // creating a new reference counted reference to synth
-        let control_thread_synth = synth_arc_mtx.clone();
-        thread::spawn(move || {
-            loop {
-                {
-                    let mut synth = control_thread_synth.lock().unwrap();
-                    synth.curr_freq = freqs[curr_freq_id];
-                }
-                voice.play();
-                curr_freq_id = (curr_freq_id+1) % freqs.len();
-                if curr_freq_id == 0 {
-                    voice.pause();
-                }
-                thread::sleep(Duration::from_millis(500));
+    // creating a new reference counted reference to synth
+    let control_thread_synth = synth_arc_mtx.clone();
+    thread::spawn(move || {
+        loop {
+            {
+                let mut synth = control_thread_synth.lock().unwrap();
+                synth.curr_freq = freqs[curr_freq_id];
             }
-        });
-    }
+            voice.play();
+            curr_freq_id = (curr_freq_id+1) % freqs.len();
+            if curr_freq_id == 0 {
+                voice.pause();
+            }
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
 
     event_loop.run();
 }
